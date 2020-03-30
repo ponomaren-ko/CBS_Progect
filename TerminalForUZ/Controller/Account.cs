5785dadea38e2fb2212f0578fd381f41f9a5b645
Bogdan <ponomaren-ko@users.noreply.github.com>
2020-03-30 09:24:39 +0300

    Всю логику переносим в библиотеку классов

---
+++ TerminalForUZ/Controller/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class Account
+    {
+        private int UserID;
+        private string name;
+        private string lastName;
+       public bool IsAdministartor { get;}
+        
+    }
+}
