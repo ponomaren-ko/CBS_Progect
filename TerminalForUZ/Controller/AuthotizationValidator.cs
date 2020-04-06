4b404b3bf06374b45a222b96f771c75dcf2530f7
Bogdan <ponomaren-ko@users.noreply.github.com>
2020-04-06 14:05:00 +0300

    Добавил класс валидатор для авторизации

---
+++ TerminalForUZ/Controller/AuthotizationValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    /// <summary>
+    /// Класс хранящий методы проверки валидности полей авторизации
+    /// </summary>
+    class AuthotizationValidator
+    {
+         public bool CheckLogin(string login)
+        {
+            return false;
+        }
+        public bool CheckPassword(string password)
+        {
+            return false;
+        }
+
+        public bool CheckEmail(string email)
+        {
+            return false;
+        }
+
+        public bool CheckPhoneNumber()
+        {
+            return false;
+        }
+    }
+}
