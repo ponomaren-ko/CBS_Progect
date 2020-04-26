58c881ea57bdd70860db9f3f8a116c94d22dffac
Bogdan <ponomaren-ko@users.noreply.github.com>
2020-04-26 10:12:23 +0300

    Удалил свойство Tickets  из Account

--- TerminalForUZ/Model/Entities/Account.cs
+++ TerminalForUZ/Model/Entities/Account.cs
@@ -19,6 +19,5 @@ namespace Controller
         public string PhoneNumber { get; set; }
         public string Login { get; set; }        
         public string Password { get; set; }
-        public Ticket[] Tickets { get; set; }
     }
 }
